22a71aaecf51b71a32e7a7870300775fd0adac15
Herrmann, Olivier (IntlAssoc) <olivier.herrmann@telecomnancy.net>
2017-02-28 16:49:31 -0500

    add missing servlet definition in web.xml add missing resources

--- docentryeditor/src/main/java/gov/nist/hit/ds/docentryeditor/server/EnvironmentServicesImpl.java
+++ docentryeditor/src/main/java/gov/nist/hit/ds/docentryeditor/server/EnvironmentServicesImpl.java
@@ -5,11 +5,7 @@ import gov.nist.hit.ds.docentryeditor.client.utils.Services.EnvironmentServices;
 import gov.nist.toolkit.installation.Installation;
 import gov.nist.toolkit.session.server.Session;
 
-import java.io.File;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 import java.util.logging.Logger;
 
 /**
@@ -17,21 +13,15 @@ import java.util.logging.Logger;
  */
 public class EnvironmentServicesImpl extends RemoteServiceServlet implements EnvironmentServices{
     private final static Logger LOGGER = Logger.getLogger(EnvironmentServicesImpl.class.getName());
-    private final static Properties PROPERTIES=new Properties();
-    private final static String PROPERTIES_FILENAME="toolkit.properties";
 
     private Session session;
 
+    // Class constructor to init ext. cache location
     public EnvironmentServicesImpl(){
-        try {
-            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(PROPERTIES_FILENAME);
-            PROPERTIES.load(inputStream);
-            Installation.instance().warHome(Installation.instance().warHome());
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
+        LOGGER.info("hello");Installation.instance().warHome(Installation.instance().warHome());
     }
 
+    // method to provide a Session object (from toolkit project, Session module)
     private Session session(){
         if (session==null){
             session = new Session(Installation.instance().warHome());
@@ -39,21 +29,39 @@ public class EnvironmentServicesImpl extends RemoteServiceServlet implements Env
         return session;
     }
 
+    /**
+     * Method that retrieves a list of existing environment names from the server (External cache).
+     * @return list of environment names
+     */
     @Override
     public List<String> retrieveEnvironmentNames() {
+        LOGGER.info("retrieve Environment Names");
         return Session.getEnvironmentNames();
     }
 
+    /**
+     * Method that retrieves a list of existing session names from the server (External cache).
+     * @return list of session names
+     */
     @Override
     public List<String> retrieveSessionNames() throws Exception {
+        LOGGER.info("retrieve session names");
         return session().xdsTestServiceManager().getMesaTestSessionNames();
     }
 
+    /**
+     * Retrieves the path to the ext. cache on the server from the toolkit.properties file in the project's war.
+     * @return file path to the ext. cache as a String.
+     */
     @Override
     public String retrieveExternalCachePathProperty() {
         return Installation.instance().propertyServiceManager().getPropertyManager().getExternalCache();
     }
 
+    /**
+     * Saves a new value for the external cache file path in the properties file.
+     * @param extCachePathText New ext. cache file path value.
+     */
     @Override
     public void saveExternalCachePath(String extCachePathText) {
         Installation.instance().propertyServiceManager().getPropertyManager().saveProperties();
